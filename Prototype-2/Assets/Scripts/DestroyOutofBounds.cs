f873d6a4a51ff6eb6dffbcd45171e9380fc00bb9
coyee1 <121971846+coyee1@users.noreply.github.com>
2023-02-07 14:14:18 -0800

    2.4

--- Prototype-2/Assets/Scripts/DestroyOutofBounds.cs
+++ Prototype-2/Assets/Scripts/DestroyOutofBounds.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOutofBounds : MonoBehaviour
-    
+
 {
+    //boundaries
     private float topBound = 30;
     private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound) {
+        if (transform.position.z > topBound)
+        {
             Destroy(gameObject);
-        }else if  (transform.position.z < lowerBound) { Destroy(gameObject);
+        }
+        //if animals past the player, remove it
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over");
         }
     }
 }
--- Prototype-2/Assets/Scripts/SpawnManager.cs
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -15,14 +15,14 @@ public class SpawnManager : MonoBehaviour
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
-    {                      
+    { }          
            void SpawnRandomAnimal() {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs [animalIndex].transform.rotation);
            ;
         }
     }
-}
+
